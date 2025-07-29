f4d331b4fe6435eb404e3cddf3a83527b4d147f6
magicenvision <rjrose2003@gmail.com>
2025-07-28 22:48:26 -0400

    configured nextjs project

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import '../app/globals.css';
+
+export default function Home() {
+  return (
+   <main className="bg-black-100 text-white">
+    <div>
+      <h1>Hello World</h1>
+    </div>
+   </main>
+  );
+}
