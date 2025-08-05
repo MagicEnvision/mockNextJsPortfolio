aba4bb4094be1e8ff771ab13ec61825c7ae109e7
magicenvision <rjrose2003@gmail.com>
2025-08-05 19:24:36 -0400

    added theme provider

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,15 +1,28 @@
 import React from 'react'
+import { cn } from "@/lib/util";
 import { Spotlight } from './ui/Spotlight'
 
 const Hero = () => {
   return (
-    <div className='pb-20 pt-36'>
-        <div>
+    <div className='pb-20 pt-36  overflow-hidden'>
+        <div className=''>
             <Spotlight className='-top-40 -left-10 md:-left-32 md:-top-20 h-screen' fill='white'/>
             <Spotlight className='top-10 -left-full h-[80vh] w-[50vw] md:-left-32 md:-top-20 ' fill='purple'/>
-            <Spotlight className='top-28 left-80 h-[80vh] w-[50vw]' fill='white'/>
-
+            <Spotlight className='top-28 left-80 h-[80vh] w-[50vw]' fill='blue'/>
         </div>
+        <div className="relative flex h-screen w-full items-center justify-center bg-white dark:bg-black">
+      <div
+        className={cn(
+          "absolute inset-0",
+          "[background-size:40px_40px]",
+          "[background-image:linear-gradient(to_right,#e4e4e7_1px,transparent_1px),linear-gradient(to_bottom,#e4e4e7_1px,transparent_1px)]",
+          "dark:[background-image:linear-gradient(to_right,#262626_1px,transparent_1px),linear-gradient(to_bottom,#262626_1px,transparent_1px)]",
+        )}
+      />
+      {/* Radial gradient for the container to give a faded look */}
+      <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black"/>
+
+    </div>
     </div>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
    <main className="bg-black text-white flex justify-center
     items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
-    <div className="max-w-7xl w-full">
+    <div className="max-w-7xl w-full relative">
       <Hero />
     </div>
    </main>
